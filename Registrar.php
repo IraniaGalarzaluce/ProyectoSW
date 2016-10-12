15b3c68cb5dc08a547b76d96a70afde04816beed
Irati <igalarza009@ikasle.ehu.eus>
2016-10-12 18:57:01 +0200

    Lab seg. parte servidor

--- Registrar.php
+++ Registrar.php
@@ -1,7 +1,88 @@
+
 <?php
 
 	$link = mysqli_connect("localhost", "root", "", "quiz");
 	//$link = mysqli_connect("mysql.hostinger.es", "u531741362_root", "iratiania", "u531741362_quiz");
+
+	function validarExpresion($variable, $expresion){
+		$validar = array(
+				 "options" => array("regexp"=>$expresion)
+			);
+		if(!filter_var($variable, FILTER_VALIDATE_REGEXP, $validar)){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	function validaRequerido($valor){
+		if(trim($valor) == ''){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	echo "<style type='text/css'>
+		<!--
+			p{text-align: center; font: small-caps bold 120% serif}
+			a{text-align : center; font: small-caps bold 120% serif}
+			body{background-color : #E6E6E6}
+		-->
+	</style>";
+	
+	if (!validaRequerido($_POST['Nombre'])) {
+		echo '<br> <br> <p> Error: Faltan campos obligatorios por rellenar. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validaRequerido($_POST['Apellidos'])){
+		echo '<br> <br> <p> Error: Faltan campos obligatorios por rellenar.  </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validarExpresion($_POST['Apellidos'], '/^[a-zA-Z]+(\s)+[a-zA-Z]+$/')){
+		echo '<br> <br> <p> Error: Debes introducir dos apellidos. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validaRequerido($_POST['Correo'])){
+		echo '<br> <br> <p> Error: Faltan campos obligatorios por rellenar. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validarExpresion($_POST['Correo'], '/^[a-z]{2,}[0-9]{3}@ikasle\.ehu\.(eus|es)$/')){
+		echo '<br> <br> <p> Error: El correo introducido no cumple el formato de la UPV/EHU. Vuelve a intentar registrarte por favor. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validaRequerido($_POST['Password'])){
+		echo '<br> <br> <p> Error: Faltan campos obligatorios por rellenar. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validaRequerido($_POST['Telefono'])){
+		echo '<br> <br> <p> Error: Faltan campos obligatorios por rellenar. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	if(!validarExpresion($_POST['Telefono'], '/^(6|7|9){1}[0-9]{8}$/')){
+		echo '<br> <br> <p> Error: Telefono no válido. </p> <br> <br>';
+		echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+		die();
+	}
+	
+	$esp = $_POST['Especialidad'];
+	if(strcmp($esp, "Otro") == 0){
+		if(!validaRequerido($_POST['otraEspecialidad'])){
+			echo '<br> <br> <p> Error: Faltan campos obligatorios por rellenar. </p> <br> <br>';
+			echo "<p> <a href='registro.html'> Volver a registro </a> </p>";
+			die();
+		}
+		$esp = $_POST['otraEspecialidad'];
+	}
 	
 	$target_dir = "uploads/";
 	$target_file = $target_dir . basename($_FILES["imagen"]["name"]);
@@ -19,31 +100,13 @@
 			$file = "avatar.jpg";
 		}
 	}
-	
-	$esp = $_POST['Especialidad'];
-	if(strcmp($esp, "Otro") == 0){
-		$esp = $_POST['otraEspecialidad'];
-	}
-	
-	$validar = array(
-             "options" => array("regexp"=>'/^[a-z]{2,}[0-9]{3}@ikasle\.ehu\.(eus|es)$/')
-        );
-	if(!filter_var($_POST['Correo'], FILTER_VALIDATE_REGEXP, $validar)){
-		die('Error: El correo introducido no cumple el formato de la UPV/EHU. Vuelve a intentar registrarte por favor.');
-	}
-	
+
 	$sql="INSERT INTO usuario VALUES ('$_POST[Nombre]', '$_POST[Apellidos]', '$_POST[Correo]', '$_POST[Password]', '$_POST[Telefono]', '$esp', '$_POST[Opcional]', '$file' )";
 
 	if (!mysqli_query($link ,$sql)){
 		die('Error: ' . mysqli_error($link));
 	}
-	echo "<style type='text/css'>
-		<!--
-			p{text-align: center; font: small-caps bold 120% serif}
-			a{text-align : center; font: small-caps bold 120% serif}
-			body{background-color : #E6E6E6}
-		-->
-	</style>
+	echo "
 	<br>
 	<br>
 	<br>";
