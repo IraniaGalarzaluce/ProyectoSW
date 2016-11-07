f32e481869805c3a43de546d1cc4ae06b882f2ab
Irati <igalarza009@ikasle.ehu.eus>
2016-11-07 19:35:16 +0100

    Lab servicios web

--- validarCorreo.php
+++ validarCorreo.php
@@ -6,7 +6,7 @@
 	//creamos el objeto de tipo soapclient.
 	//http://www.mydomain.com/server.php se refiere a la url
 	//donde se encuentra el servicio SOAP que vamos a utilizar.
-	$soapclient = new nusoap_client( 'http://sw14.hol.es/ServiciosWeb/comprobarmatricula.php?wsdl',true);
+	$soapclient = new nusoap_client( 'http://sw14.hol.es/ServiciosWeb/comprobarmatricula.php',false);
 	
 	$email = $_GET['email'];
 	$result = $soapclient->call('comprobar', array('x'=>$email));
